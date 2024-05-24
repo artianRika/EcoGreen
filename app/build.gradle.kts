858399867cfffa323fd9cb50d403f95424759d41
Artian RIKA <artianrika@gmail.com>
2024-05-24 14:20:58 +0200

    navbar working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.gms.google-services")
 }
 
 android {
--- app/src/main/java/com/underoid/ecogreen/MainActivity.kt
+++ app/src/main/java/com/underoid/ecogreen/MainActivity.kt
@@ -2,11 +2,40 @@ package com.underoid.ecogreen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.underoid.ecogreen.fragments.AccountFragment
+import com.underoid.ecogreen.fragments.HomeFragment
+import com.underoid.ecogreen.fragments.MapFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val homeFragment = HomeFragment()
+        val mapFragment = MapFragment()
+        val accountFragment = AccountFragment()
+
+    val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+
+        bottomNavView.getMenu().getItem(0).setChecked(true);
+        bottomNavView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.homeFragment ->setCurrentFragment(homeFragment)
+                R.id.mapFragment -> setCurrentFragment(mapFragment)
+                R.id.accountFragment -> setCurrentFragment(accountFragment)
+            }
+            true
+        }
+
+    }
+
+
+    private fun setCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, fragment)
+            commit()
+        }
     }
 }
